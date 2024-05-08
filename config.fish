d11b9713420e8a4a6cf8ee47b36469dba7e8db97
feebrian <adamaditama13@gmail.com>
2024-05-08 07:28:18 +0700

    conf: add zoxide
    bring zoxide to the terminal

--- config.fish
+++ config.fish
@@ -5,6 +5,7 @@ export PATH="$(yarn global bin):$PATH"
 
 # aliases
 alias q="exit"
+alias ls="exa -l"
 alias die="shutdown now"
 alias arise="reboot"
 
@@ -12,10 +13,14 @@ alias c="code"
 alias ch="code ."
 alias v="nvim"
 alias vh="nvim ."
+alias cd="z"
+
+# command config aliases
 alias fc="nvim ~/.config/fish/"
 alias fs="source ~/.config/fish/config.fish"
 alias kf="nvim ~/.config/kitty/kitty.conf"
 alias vc="nvim ~/.config/nvim"
+alias ac="nvim ~/.config/alacritty"
 
 alias nrd="npm run dev"
 alias nrb="npm run build"
